19c400262c11154ebc3c8b4cebfc1805a0c87f3b
Motepalli Preethi <45934300+MotepalliPreethi@users.noreply.github.com>
2024-01-22 22:43:20 +0000

    Basic home page is created

--- Aadi-travel-website/app.py
+++ Aadi-travel-website/app.py
@@ -1,12 +1,16 @@
+import sqlite3
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/static')
+
+def get_db_connection():
+    conn = sqlite3.connect('travel-website.db')
+
+
 
 @app.route('/')
 def home():
-    return render_template('../templates/home.html')
-
-
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
